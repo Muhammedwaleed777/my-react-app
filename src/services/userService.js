437a7fb39bc101a15c1f3e446ada071a76e9f3ed
Muhammedwaleed777 <fa24-bse-002@students.cuisahiwal.edu.pk>
2026-08-25 10:02:01 +0500

    Updates

--- src/services/userService.js
+++
@@ -1,8 +0,0 @@
-import { api } from './api';
-
-export const userService = {
-  getAll: () => api.get('/users'),
-  getById: (id) => api.get(`/users/${id}`),
-  login: (email, password) => api.post('/auth/login', { email, password }),
-  register: (data) => api.post('/auth/register', data),
-};
---
+++ static/js/488.12700aab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkmy_first_app||=[]).push([[488],{488(){new Set,new WeakMap}}]);
+//# sourceMappingURL=488.12700aab.chunk.js.map
